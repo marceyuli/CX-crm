c83b5fb3eeb653d32e74dd88172f67a2bdfb180b
Yerireyes <le_soleto@hotmail.com>
2022-09-01 13:31:12 -0400

    .

--- app.js
+++ app.js
@@ -196,8 +196,8 @@ function handleDialogFlowResponse(sender, response) {
 
   if (isDefined(action)) {
     handleDialogFlowAction(sender, action, messages, contexts, parameters);
-  // } else if (isDefined(messages)) {
-  //   handleMessages(messages, sender);
+  } else if (isDefined(messages)) {
+    handleMessages(messages, sender);
   } else if (responseText == "" && !isDefined(action)) {
     //dialogflow could not evaluate input.
     sendTextMessage(sender, "No entiendo lo que trataste de decir ...");
