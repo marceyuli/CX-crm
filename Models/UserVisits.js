12fde8c872e14715c9ac09b7d8c2264954981da9
jpceci <cecilia_jp@live.com>
2022-10-08 12:45:52 -0400

    .

--- Models/UserVisits.js
+++ Models/UserVisits.js
@@ -3,6 +3,7 @@ const Schema = mongoose.Schema;
 
 const UserVisitsSchema = new Schema({
     chatBotUserId: mongoose.SchemaTypes.ObjectId,
+    comment: String,
 }, { timestamps: true });
 
 module.exports = mongoose.model('UserVisits', UserVisitsSchema);
