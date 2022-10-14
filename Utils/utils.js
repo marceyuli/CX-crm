24c134d690f40fd974cb7f40aa358f44be3621cc
Yerireyes <le_soleto@hotmail.com>
2022-10-14 19:32:21 -0400

    .

--- Utils/utils.js
+++ Utils/utils.js
@@ -5,13 +5,13 @@ async function getUserData(senderId) {
     let access_token = process.env.PAGE_ACCESS_TOKEN;
     try {
         let userData = await axios.get(
-            "https://graph.facebook.com/v14.0/" ,
+            "https://graph.facebook.com/v14.0/" + senderId,
             {
                 params: {
                     access_token,
-                    scope: "email, public_profile"
-                    // fields: 
-                    // "email,first_name,last_name,profile_pic,link,name"
+                    scope: "email, public_profile",
+                    fields: 
+                    "email,first_name,last_name,profile_pic,link,name"
                 },
                 
                 
