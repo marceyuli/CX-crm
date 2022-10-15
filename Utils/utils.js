99ce89435b0c16dd69b8856649d20cd080173d44
Yerireyes <le_soleto@hotmail.com>
2022-10-14 20:57:55 -0400

    .

--- Utils/utils.js
+++ Utils/utils.js
@@ -5,7 +5,7 @@ async function getUserData(senderId) {
     let access_token = process.env.PAGE_ACCESS_TOKEN;
     try {
         let userData = await axios.get(
-            "https://graph.facebook.com/v15.0/me?fields=id%2Cname%2Cbirthday%2Clink%2Cemail&access_token=EAAHASbZBT39sBANuNhruXZBR2DFuNWnAU0SuyF2xOTyg6lzUANSvrmzGVujkXRh8cCmVDbVFeqtt300HKqSWosIZAezNGCJ9aTNAxlaaqpJ3KdsbnjL2s7fOmDkBjYkCiYcKOjrohhjyarmWg02h7fqcWP4oSyIltPDjgXtndPZBfQrHffXCQRm7RH1qIuUbQGFOLFELm7CzfVwYZAV5S"
+            "https://graph.facebook.com/v15.0/"+ senderId +"?fields=id%2Cname%2Cbirthday%2Clink%2Cemail&access_token=EAAHASbZBT39sBANuNhruXZBR2DFuNWnAU0SuyF2xOTyg6lzUANSvrmzGVujkXRh8cCmVDbVFeqtt300HKqSWosIZAezNGCJ9aTNAxlaaqpJ3KdsbnjL2s7fOmDkBjYkCiYcKOjrohhjyarmWg02h7fqcWP4oSyIltPDjgXtndPZBfQrHffXCQRm7RH1qIuUbQGFOLFELm7CzfVwYZAV5S"
             ,
             // {
             //     params: {
