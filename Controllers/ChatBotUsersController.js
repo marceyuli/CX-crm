1f7d9a8095ae3b3561ace39e099d93aecca75ae5
Yerireyes <le_soleto@hotmail.com>
2022-10-28 20:20:50 -0400

    prueba getTimesVisited

--- Controllers/ChatBotUsersController.js
+++ Controllers/ChatBotUsersController.js
@@ -27,35 +27,6 @@ let getUsersData = async (req, res) => {
     let chatBotUsers = await ChatbotUser.find({ state: data.state });
     console.log(chatBotUsers);
     res.json(chatBotUsers);
-    // let chatBotUsers = await ChatbotUser.find()
-    // let newChatBotUsers = [];
-    // let newChatBotUsers1 = [];
-    // let newChatBotUsers2 = [];
-    // let newChatBotUsers3 = [];
-    // let newChatBotUsers4 = [];
-    // chatBotUsers.forEach(element => {
-    //     switch (element.state) {
-    //         case 1:
-    //             newChatBotUsers1.push(element);
-    //             break;
-    //         case 2:
-    //             newChatBotUsers2.push(element);
-    //             break;
-    //         case 3:
-    //             newChatBotUsers3.push(element);
-    //             break;
-    //         case 4:
-    //             newChatBotUsers4.push(element);
-    //             break;
-    //         default:
-    //             break;
-    //     }
-    // });
-    // newChatBotUsers.push(newChatBotUsers1);
-    // newChatBotUsers.push(newChatBotUsers2);
-    // newChatBotUsers.push(newChatBotUsers3);
-    // newChatBotUsers.push(newChatBotUsers4);
-    // res.json(newChatBotUsers);
 }
 
 // async function getUsersByState(state) {
@@ -63,8 +34,7 @@ let getUsersData = async (req, res) => {
 // }
 
 let getTimesVisited = async (req, res) => {
-    var data = req.body;
-    console.log(data);
+    var data = req.query;
     let chatBotUsersTimesVisited = (await ChatbotUser.find({ _id: data._id })).length
     console.log(chatBotUsersTimesVisited);
     res.json(chatBotUsersTimesVisited);
