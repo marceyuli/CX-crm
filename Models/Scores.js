7a31827b8938617f1229713a9525b1d9211d554f
Yerireyes <le_soleto@hotmail.com>
2022-10-31 15:41:22 -0400

    schema de Score cambiado de 'punctuation' a 'score'

--- Models/Scores.js
+++ Models/Scores.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const ScoreSchema = new Schema({
-    punctuation: Number,
+    score: Number,
     chatBotUserId: mongoose.SchemaTypes.ObjectId,
 }, { timestamps: true });
 
