82f07830724e106132a0ec51c051906271641934
jpceci <cecilia_jp@live.com>
2022-10-31 11:15:21 -0400

    getListShopppingCart casi terminado

--- Controllers/Products_OrdersController.js
+++ Controllers/Products_OrdersController.js
@@ -22,9 +22,71 @@ async function createProductsOrders(facebookId, productName, productType, quanti
         console.log("Se creo un product order: ", res);
     })
 }
-
+// se fue la luz en todo el barrio, prende las velas que esta fiesta no se apaga
 async function getListShoppingCart(facebookId) {
     let chatBotUser = await ChatBotUsers.findOne({ facebookId });
+    let shoppingCart = await Order.aggregate([
+        {
+            $match: {
+                chatBotUserId: chatBotUser._id,
+                order: false
+            }
+        },
+        {
+            $lookup: {
+                from: "products_orders",
+                localField: "_id",
+                foreignField: "orderId",
+                pipeline: [
+                    {
+                        $lookup: {
+                            from: "products",
+                            localField: "productId",
+                            foreignField: "_id",
+                            pipeline: [
+                                {
+                                    $project: {
+                                        _id: 0,
+                                        name: 1,
+                                        type: 1,
+                                        picture: 0,
+                                        price: 0,
+                                        artistId: 0
+                                    }
+                                },
+                            ],
+                            as: "product",
+                        }
+                    },
+                    {
+                        $replaceRoot: {
+                            newRoot:
+                            {
+                                $mergeObjects:
+                                    [
+                                        {
+                                            $arrayElemAt:
+                                                ["$product", 0]
+                                        }, "$$ROOT"]
+                            }
+                        },
+                    },
+                    {
+                        $project:
+                            { product: 0 }
+                    }
+                ],
+                as: "product_orders"
+            }
+        },
+        {
+            $unwind: "$product_orders"
+        }
+    ]);
+    let res = "Actualmente tienes lo siguiente en tu carrito de compras:\n";
+    shoppingCart.forEach(element => {
+        res += element.quantity + " " + element.type + " "  + element.name + " de talla " + element.size 
+    });
 }
 
 module.exports = {
