f92b8616da56de7d033e9b3fc0e617ee280b7f26
Yerireyes <le_soleto@hotmail.com>
2022-10-31 23:29:28 -0400

    prueba api getTalkDetails para clientes contactados

--- Controllers/TalkDetailsController.js
+++ Controllers/TalkDetailsController.js
@@ -1,6 +1,5 @@
 const TalkDetails = require('../Models/TalkDetails');
 const ChatbotUsers = require('../Models/ChatbotUsers');
-const ChatBotUsersController = require('./ChatBotUsersController');
 
 async function saveTalkDetail(content, socialMedia, chatBotUserId) {
     let talkDetail = new TalkDetails({
@@ -81,7 +80,7 @@ let getTalkDetails = async (req, res) => {
     try {
         let data = req.body;
         console.log(data);
-        let chatBotUser = await ChatBotUsersController.getChatBotUser(data.facebookId);
+        let chatBotUser = await ChatbotUsers.findOne({ facebookId: data.facebookId});
         let talkdetails = await TalkDetails.find({chatBotUserId:chatBotUser._id});
         console.log(talkdetails);
         res.json(talkdetails);
